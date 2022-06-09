b47695a8a7f5e373ca6c292f5f60f3ef4f09cc50
luancloudmis <caothanhluan91298@gmail.com>
2022-06-09 23:09:44 +0700

    init

---
+++ apps/todo/src/app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-mfe-root',
+  template: '<router-outlet></router-outlet>'
+})
+export class AppComponent {}
---
+++ apps/todo/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-mfe-todo-entry',
+  template: `<ng-mfe-nx-welcome></ng-mfe-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
